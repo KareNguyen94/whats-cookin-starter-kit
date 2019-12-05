126c9d28d6d9d1a800a09460bbfe0780bebde486
Ryan Novak <ryan.novak95@yahoo.com>
2019-12-04 17:52:15 -0700

    Wrote tests for pantry class

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -4,16 +4,27 @@ const Pantry = require('../src/pantry-class');
 const usersData = require('../data/users');
 
 
-describe('ingredients', function() {
+describe('ingredients', function () {
 
-  it('should be a function', function() {
+  let pantry;
+
+  beforeEach(() => {
+    pantry = new Pantry(usersData[0]);
+  });
+
+  it('should be a function', function () {
     expect(Pantry).to.be.a('function');
   });
 
-  it('should be an instance of Pantry', function() {
-    const pantry = new Pantry();
-    const user = new User(usersData[0]);
-    console.log(pantry)
+  it('should be an instance of Pantry', function () {
     expect(pantry).to.be.an.instanceof(Pantry);
+  });
+
+  it('should have a current stock that holds the users pantry information', function () {
+    expect(pantry.currentStock).to.deep.equal(usersData[0].pantry);
+  });
+
+  it('should default to having enough items in stock', function () {
+    expect(pantry.enoughItems).to.equal(true);
   });
 });
