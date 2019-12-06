23198ca25241bd8c025349c7e8d71a1a2f86ff1d
Karen Nguyen <knguyen4184@yahoo.com>
2019-12-06 15:51:27 -0700

    Start favorite functionality

--- src/scripts.js
+++ src/scripts.js
@@ -10,14 +10,27 @@ function instantiateRecipes() {
 };
 
 function insertInstructions(steps) {
-    const instructionList = steps.map(step => `<li>${step.number}- ${step.instruction}</li>`);
-    console.log(instructionList)
+    const instructionList = steps.map(step => `<li>${step.number}. ${step.instruction}</li>`);
     return instructionList.join('\n');
 }
 
 function insertIngredients(items) {
-    const ingredientList = items.map(item => `<li>${item.quantity.amount} ${item.quantity.unit} ${item.name}</li>`);
+    const ingredientList = items.map(item => `<li>-${item.quantity.amount} ${item.quantity.unit} ${item.name}</li>`);
     return ingredientList.join('\n');
+}
+
+function onCardClick(event) {
+  var recipeCard = event.target.parentNode.parentNode;
+  recipes.forEach(selectedRecipe => {
+    if(recipes[i].id === id) {
+      addToFavorite(recipes[i])
+    }
+  })
+
+}
+
+function addToFavorite(recipes) {
+  recipes.addFavoriteRecipe(recipe);
 }
 
 function displayRecipeCards() {
@@ -29,18 +42,18 @@ function displayRecipeCards() {
       <img class="food-image" src="${allRecipes[i].image}" alt="food">
       <p class="recipe-name">${allRecipes[i].name}</p>
       <div class="recipe-list">
-        <h3>Ingredients</h3>
-        <ul>${insertIngredients(allRecipes[i].ingredients)}</ul>
-        <h3>Instructions</h3>
+        <h3>Ingredients:</h3>
+        <ul>${insertIngredients(allRecipes[i].ingredients)}</ul></br>
+        <h3>Instructions:</h3>
         <ol>${insertInstructions(allRecipes[i].instructions)}</ol>
       </div>
       <div class="card-footer">
-        <img class="cook-icon" src="https://cdn3.iconfinder.com/data/icons/rounded-white-housekeeping/139/Cooking-RoundedWhite-512.png" alt=“Cook icon”>
-        <img class="favorite-icon" src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png" alt=“Favorite icon”>
+        <button><img class="cook-button" src="https://cdn3.iconfinder.com/data/icons/rounded-white-housekeeping/139/Cooking-RoundedWhite-512.png" alt=“Cook icon”></button>
+        <button><img class="favorite-button" src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png" alt=“Favorite icon”></button>
       </div>
     </article>`
-        )
-    }
+    )
+  }
 };
 
 window.onload = displayRecipeCards();
