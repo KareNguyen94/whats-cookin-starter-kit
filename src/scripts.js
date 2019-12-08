9d9eaa49e4998534cfea971edba816223cf79f9a
Ryan Novak <ryan.novak95@yahoo.com>
2019-12-07 23:32:01 -0700

    Added functions to properly display recipes saved in favorites and to cook array

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,91 @@
 var recipeCardSection = document.querySelector('.recipe-card-section');
+var headerSection = document.querySelector('.user-header');
 let recipes = [];
 let user;
 let pantry;
 let cardId;
 let selectedCard;
-let numberrrr;
+let randomNumber;
 
 
 window.onload = displayRecipeCards();
 
 recipeCardSection.addEventListener("click", handleCardClick);
+headerSection.addEventListener("click", handleHeaderClick);
+
+function handleHeaderClick() {
+    if (event.target.classList.contains('home-page-button')) {
+        displayHomeCards();
+    } if (event.target.classList.contains('favorite-page-button')) {
+        displayFavoriteCards();
+    } if (event.target.classList.contains('to-cook-page-button')) {
+        displayToCookCards();
+    }
+}
+
+function displayHomeCards() {
+    recipeCardSection.innerHTML = '';
+    for (let i = 0; i < allRecipes.length; i++) {
+        recipeCardSection.insertAdjacentHTML('beforeend',
+            `<article class="card" id=${allRecipes[i].id}>
+      <img class="food-image" src="${allRecipes[i].image}" alt="food">
+      <p class="recipe-name">${allRecipes[i].name}</p>
+      <div class="recipe-list">
+        <h3>Ingredients:</h3>
+        <ul>${insertIngredients(allRecipes[i].ingredients)}</ul></br>
+        <h3>Instructions:</h3>
+        <ol>${insertInstructions(allRecipes[i].instructions)}</ol>
+      </div>
+      <div class="card-footer">
+        <button class="cook-button"></button>
+        <button class="favorite-button"></button>
+      </div>
+    </article>`
+    )}
+}
+
+
+function displayFavoriteCards() {
+    recipeCardSection.innerHTML = '';
+    for (let i = 0; i < user.favoriteRecipes.length; i++) {
+        recipeCardSection.insertAdjacentHTML('beforeend',
+            `<article class="card" id=${user.favoriteRecipes[i].id}>
+      <img class="food-image" src="${user.favoriteRecipes[i].image}" alt="food">
+      <p class="recipe-name">${user.favoriteRecipes[i].name}</p>
+      <div class="recipe-list">
+        <h3>Ingredients:</h3>
+        <ul>${insertIngredients(user.favoriteRecipes[i].ingredients)}</ul></br>
+        <h3>Instructions:</h3>
+        <ol>${insertInstructions(user.favoriteRecipes[i].instructions)}</ol>
+      </div>
+      <div class="card-footer">
+        <button class="cook-button"></button>
+        <button class="favorite-button"></button>
+      </div>
+    </article>`
+        )}
+}
+
+function displayToCookCards() {
+    recipeCardSection.innerHTML = '';
+    for (let i = 0; i < user.recipesToCook.length; i++) {
+        recipeCardSection.insertAdjacentHTML('beforeend',
+            `<article class="card" id=${user.recipesToCook[i].id}>
+      <img class="food-image" src="${user.recipesToCook[i].image}" alt="food">
+      <p class="recipe-name">${user.recipesToCook[i].name}</p>
+      <div class="recipe-list">
+        <h3>Ingredients:</h3>
+        <ul>${insertIngredients(user.recipesToCook[i].ingredients)}</ul></br>
+        <h3>Instructions:</h3>
+        <ol>${insertInstructions(user.recipesToCook[i].instructions)}</ol>
+      </div>
+      <div class="card-footer">
+        <button class="cook-button"></button>
+        <button class="favorite-button"></button>
+      </div>
+    </article>`
+        )}
+}
 
 function handleCardClick() {
   if (event.target.classList.contains('favorite-button')) {
@@ -36,7 +112,7 @@ function cookClick() {
 
 
 function getRandomInt(min, max) {
-    numberrrr = Math.floor(Math.random() * (max - min + 1)) - 1;
+    randomNumber = Math.floor(Math.random() * (max - min + 1)) - 1;
 }
 
 function instantiateRecipes() {
@@ -48,7 +124,7 @@ function instantiateRecipes() {
 };
 
 function instantiateUser() {
-    user = new User(usersData[numberrrr]);
+    user = new User(usersData[randomNumber]);
     return user;
 };
 
@@ -85,11 +161,12 @@ function onCookButtonClick(cardId) {
   user.addToCook(selectedCard);
 }
 
+
+
 function displayRecipeCards() {
     instantiateRecipes();
     allRecipes = instantiateRecipes();
-    getRandomInt(1, 49);
-    console.log(numberrrr);
+    getRandomInt(1, 48);
     for (let i = 0; i < allRecipes.length; i++) {
         recipeCardSection.insertAdjacentHTML('beforeend',
     `<article class="card" id=${allRecipes[i].id}>
