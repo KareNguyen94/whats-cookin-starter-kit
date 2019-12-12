961339efa7cb52a9b9d09aa709bcc67cf7b505d6
Ryan Novak <ryan.novak95@yahoo.com>
2019-12-11 19:02:40 -0700

    Added functionality that compares recipe ingredients to ingredients within users pantry

--- src/pantry-class.js
+++ src/pantry-class.js
@@ -3,15 +3,30 @@ class Pantry {
     this.id = user.id;
     this.currentStock = user.pantry;
     this.itemsNeeded = [];
-    this.enoughItems = true;
   }
 
-  checkItemStock() {
-
+  checkItemStock(recipe) {
+    recipe.ingredients.forEach(recipe => {
+      let ingredientName = recipe.name;
+      const userPantryItem = this.currentStock.find(ingredient => ingredient.ingredient === recipe.id);
+      if (!userPantryItem) {
+        return this.itemsNeeded.push({[ingredientName]: recipe.quantity.amount})
+      }
+      if (recipe.quantity.amount > userPantryItem.amount) {
+        let amountNeeded = ((userPantryItem.amount - recipe.quantity.amount) * -1);
+        this.itemsNeeded.push({[ingredientName]: amountNeeded})
+      }
+    })
+    if (this.itemsNeeded.length > 0) {
+      this.checkCost();
+    } else {
+      displayInstructions();
+    }
   }
-
+      
+      
   checkCost() {
-
+    console.log('COST')
   }
 }
 
